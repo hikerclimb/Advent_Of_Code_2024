3ddf19a85e454d05c47945cb68d37c1ba3c67f7b
Aniket Karmarkar <karmarkaraniket@gmail.com>
2025-01-02 16:12:44 -0600

    problem 1 day 2 completed

---
+++ Day2Problem1/Day2Problem1.py
@@ -0,0 +1,36 @@
+def main():
+    with open("input.txt", "r") as file:
+        arr = []
+        dcounter = False
+        acounter = False
+        diff = 0
+        prevDirection = 0
+        countDCounter = 0
+        countACounter = 0
+        for y in file:
+            input = y.split()
+            for xid,x in enumerate(input[0:len(input)]):
+                diff = abs(int(input[xid]) - int(input[xid-1]))
+                if((int(input[xid]) < int(input[xid-1])) and diff >= 1 and diff <=3):
+                    countDCounter = countDCounter + 1
+                elif((int(input[xid]) > int(input[xid-1])) and diff >= 1 and diff <=3):
+                    countACounter = countACounter + 1
+            if(countACounter == (len(input) - 1)):
+                acounter = True
+            elif(countDCounter == (len(input) - 1)):
+                dcounter = True
+            if(acounter == True or dcounter == True):
+                arr.append("1")
+            else:
+                arr.append("0")
+            countACounter = 0
+            countDCounter = 0
+            dcounter = False
+            acounter = False
+        summation = 0
+        for num in arr:
+            numFromArray = int(num)
+            summation = summation + numFromArray
+        print(summation)
+if __name__ == "__main__":
+    main()
